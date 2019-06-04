44fbc6febc584eddd6ee645735314b8a9c0ee24e
mahboobehad <mhstar32@gmail.com>
2019-06-04 11:35:42 +0430

    solved the problem :D

--- perceptron_net.py
+++ perceptron_net.py
@@ -1,8 +1,6 @@
 # In the name of God
 import numpy as np
-import re
-
-from utils import bipolar
+from utils import bipolar, generate_data_set_from_file
 
 
 class OneLayerPerceptronNet:
@@ -29,33 +27,6 @@ class OneLayerPerceptronNet:
 
     def __repr__(self):
         return '{0!s} {0!r}'.format(self.__dict__)
-
-
-def generate_data_set_from_file(file_path, target_character='A'):
-    def read_file():
-        with open(file_path) as f:
-            lines = f.readlines()
-            return ''.join(_ for _ in lines)
-
-    map_pattern = re.compile('[\w:\s\t,]+[.#\s]+', re.I | re.M)
-    maps = map_pattern.findall(read_file())
-
-    data = list()
-    target = list()
-
-    title_pattern = re.compile('[\w\s,:]+', re.I | re.M)
-    map_pattern = re.compile('[.#]+')
-
-    for m in maps:
-        m = m.strip()
-        title = title_pattern.search(m).group(0).strip()
-        target.append(np.array([1]) if target_character == title[~0] else np.array([-1]))
-
-        map_ = map_pattern.findall(m)
-        data.append(np.array(list(map(lambda x: 0 if x == '.' else 1, map_))))
-
-    re.purge()
-    return data, target
 
 
 def solve_first_hw():
--- utils.py
+++ utils.py
@@ -1,6 +1,37 @@
+# In the name of God
+import re
+
+import numpy as np
+
+
 def ac_func(x):
     return -1 if x < .5 else 0 if -.5 <= x <= .5 else 1
 
 
 def bipolar(x):
     return -1 if x < 0 else 1
+
+
+def generate_data_set_from_file(file_path, target_character='A'):
+    def read_file():
+        with open(file_path) as f:
+            lines = f.readlines()
+            return ''.join(_ for _ in lines)
+
+    map_pattern = re.compile('[\w:\s\t,]+[.#\s]+', re.I | re.M)
+    maps = map_pattern.findall(read_file())
+
+    data = list()
+    target = list()
+
+    title_pattern = re.compile('[\w\s,:]+', re.I | re.M)
+    map_pattern = re.compile('[.#]+')
+
+    for m in maps:
+        m = m.strip()
+        title = title_pattern.search(m).group(0).strip()
+        target.append(np.array([1]) if target_character == title[~0] else np.array([-1]))
+        map_ = map_pattern.findall(m)
+        data.append(np.array(list(map(lambda x: 0 if x == '.' else 1, map_))))
+    re.purge()
+    return data, target
