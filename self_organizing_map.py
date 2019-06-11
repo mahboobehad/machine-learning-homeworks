43b9322f5e6364b945bc4902cf60fe97add14c58
mahboobehad <mhstar32@gmail.com>
2019-06-11 19:45:48 +0430

    som works with both one and two dim clusters. solved first hw using this net.

--- self_organizing_map.py
+++ self_organizing_map.py
@@ -4,39 +4,51 @@ from math import sqrt
 
 import numpy as np
 
-from utils import geometric_series, generate_data_set_from_file, linear_neighbourhood, data_to_map
+from utils import geometric_series, generate_data_set_from_file, diamond_neighbourhood
 
 
 class SOM:
-    def __init__(self, number_of_clusters, learning_rate_generator, neighbourhood_function):
+    def __init__(self, number_of_clusters, learning_rate_generator, neighbourhood_function, number_of_dimensions=1,
+                 rows=None, columns=None):
         self.weights = None
         self.number_of_clusters = number_of_clusters
         self.learning_rate_generator = learning_rate_generator
         self.neighbourhood_function = neighbourhood_function
-        self.neighbourhood_radius = self._max_count_of_neighbours()
+        self.number_of_dimensions = number_of_dimensions
+        self.neighbourhood_radius = self._max_count_of_neighbours(number_of_dimensions)
+        self.rows = rows
+        self.columns = columns
 
     def learn(self, input_data):
         self.weights = np.random.random_sample([input_data[0].shape[0], self.number_of_clusters]).T
         iteration_counter = 0
         while True:
             iteration_counter += 1
-            print(f"iteration -> {iteration_counter}")
-            old_weight = np.copy(self.weights)
+            alpha = next(self.learning_rate_generator)
             for data in input_data:
-                cluster_index = self.find_nearest_cluster(data)
-                self._update_weights(cluster_index, data)
+                winner_cluster = self.find_nearest_cluster(data)
+                self._update_weights(winner_cluster, data, alpha)
 
-            if self._is_net_converged(old_weight, epsilon=.00001) and iteration_counter == 100:
-                print(f"converged")
+            if iteration_counter == 200:
                 break
+            if not iteration_counter % 10:
+                self.neighbourhood_radius -= 1
+                if self.neighbourhood_radius <= 0:
+                    self.neighbourhood_radius = 1
 
-    def _update_weights(self, cluster_index, data):
-        print(f'max number of neighbours is {self.max_neighbourhood_radius}')
-        for neighbour in self.neighbourhood_function(cluster_index, radius=self.max_neighbourhood_radius):
-            index = cluster_index + neighbour
-            if not (0 < index < self.weights.shape[0]):
-                break
-            self._update_weight_column(index, data)
+    def _update_weights(self, cluster_index, data, alpha):
+        if self.number_of_dimensions == 1:
+            neighbours = self.neighbourhood_function = self.neighbourhood_function(cluster_index, radius=1)
+            for neighbour in neighbours:
+                index = cluster_index + neighbour
+                if not (0 < index < self.weights.shape[0]):
+                    continue
+                self._update_weight_column(index, data, alpha)
+        else:
+            neighbours = self.neighbourhood_function(cluster_index, self.columns,
+                                                     self.rows)
+            for neighbour in neighbours:
+                self._update_weight_column(neighbour, data, alpha)
 
     def find_nearest_cluster(self, data):
         cluster_index = None
@@ -52,12 +64,11 @@ class SOM:
         distance = np.sum((data - self.weights[cluster_index]) ** 2)
         return distance
 
-    def _update_weight_column(self, cluster_index, data):
-        alpha = next(self.learning_rate_generator)
+    def _update_weight_column(self, cluster_index, data, alpha):
         self.weights[cluster_index] = self.weights[cluster_index] + alpha * (data - self.weights[cluster_index])
 
-    def _is_net_converged(self, old_weights, epsilon=0):
-        return not np.any(np.abs(self.weights - old_weights) > epsilon)
+    def _is_net_converged(self, old_weights, epsilon: float = 0):
+        return np.all(np.abs(self.weights - old_weights) < epsilon)
 
     def _max_count_of_neighbours(self, number_of_dimensions):
         if number_of_dimensions == 1:
@@ -68,9 +79,10 @@ class SOM:
             raise Exception(f'number of dimensions must be one or two, got {number_of_dimensions}.')
 
 
-if __name__ == '__main__':
-    som = SOM(7, geometric_series(a_0=.6, q=.25), neighbourhood_function=linear_neighbourhood)
-    data, _ = generate_data_set_from_file(file_path='input/hw1_input.txt')
+def cluster_characters():
+    som = SOM(21, geometric_series(.8, .05), neighbourhood_function=diamond_neighbourhood, number_of_dimensions=2,
+              rows=7, columns=3)
+    data, _, font_info = generate_data_set_from_file(file_path='input/hw1_input.txt', font_info=True)
     for d in data:
         d = d.reshape((d.shape[0], 1))
 
@@ -78,5 +90,12 @@ if __name__ == '__main__':
     clusters = defaultdict(list)
     for i, d in enumerate(data):
         cluster = som.find_nearest_cluster(d)
-        clusters[chr(ord('A') + cluster)].append(i)
-    print(clusters.keys())
+        clusters[cluster].append(font_info[i])
+    for c in clusters:
+        print(f'cluster is {c} with {len(clusters[c])} elements')
+        for d in clusters[c]:
+            print(d)
+
+
+if __name__ == '__main__':
+    cluster_characters()
--- utils.py
+++ utils.py
@@ -12,7 +12,8 @@ def binary(x):
     return -1 if x < 0 else 1
 
 
-def generate_data_set_from_file(file_path, target_character='A', input_map=lambda x: 0 if x == '.' else 1):
+def generate_data_set_from_file(file_path, target_character='A', input_map=lambda x: 0 if x == '.' else 1,
+                                font_info=False):
     def read_file():
         with open(file_path) as f:
             lines = f.readlines()
@@ -26,14 +27,17 @@ def generate_data_set_from_file(file_path, target_character='A', input_map=lambd
 
     title_pattern = re.compile('[\w\s,:]+', re.I | re.M)
     map_pattern = re.compile('[.#]+')
-
+    font_info_list = list()
     for m in maps:
         m = m.strip()
         title = title_pattern.search(m).group(0).strip()
+        font_info_list.append(title.split(':')[~0].strip())
         target.append(np.array([1]) if target_character == title[~0] else np.array([-1]))
         map_ = map_pattern.findall(m)
         data.append(np.array(list(map(input_map, map_))))
     re.purge()
+    if font_info:
+        return data, target, font_info_list
     return data, target
 
 
@@ -57,8 +61,23 @@ def linear_neighbourhood(center, radius):
     return [center + r for r in range(-radius, radius + 1)]
 
 
-def rectangular_neighbourhood(center, radius):
-    return [center + i + j for i in range(-radius, radius + 1) for j in range(-radius, radius + 1)]
+def diamond_neighbourhood(center, number_of_columns, number_of_rows):
+    def find_point():
+        # this function solves m * j + i = center
+        for i in range(number_of_rows):
+            for j in range(number_of_columns):
+                if number_of_rows * j + i == center:
+                    return i, j
+
+    def is_in_matrix(p):
+        return 0 <= p[0] < number_of_rows and 0 <= p[1] < number_of_columns
+
+    x, y = find_point()
+
+    neighbours = list(
+        map(lambda p: number_of_rows * p[1] + p[0],
+            list(filter(is_in_matrix, [(x + 1, y), (x - 1, y), (x, y), (x, y + 1), (x, y - 1)]))))
+    return neighbours
 
 
 def data_to_map(data):
@@ -71,3 +90,7 @@ def data_to_map(data):
         if (i + 1) % 7 == 0:
             units_repr += '\n'
     return units_repr.lstrip()
+
+
+if __name__ == '__main__':
+    print(diamond_neighbourhood(12, 3, 7))
